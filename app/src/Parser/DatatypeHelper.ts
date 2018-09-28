cdc0342d532a656772c9f70e5ac309876630a170
arthurKuro <arthur.geneau@gmail.com>
2018-09-28 11:39:25 -0400

    datatype to string

--- app/src/Parser/DatatypeHelper.ts
+++ app/src/Parser/DatatypeHelper.ts
@@ -19,12 +19,12 @@ export class DatatypeHelper {
         else if(str === "INT2") return Datatype.INT2;
         else if(str === "INT8") return Datatype.INT8;
         // Text
-        else if(str === "CHARACTER_20") return Datatype.CHARACTER_20;
-        else if(str === "VARCHAR_255") return Datatype.VARCHAR_255;
-        else if(str === "VARYING_CHARACTER_255") return Datatype.VARYING_CHARACTER_255;
-        else if(str === "NCHAR_55") return Datatype.NCHAR_55;
-        else if(str === "NATIVE_CHARACTER_70") return Datatype.NATIVE_CHARACTER_70;
-        else if(str === "NVCHAR_100") return Datatype.NVCHAR_100;
+        else if(str === "CHARACTER(20)") return Datatype.CHARACTER_20;
+        else if(str === "VARCHAR(255)") return Datatype.VARCHAR_255;
+        else if(str === "VARYING CHARACTER(255)") return Datatype.VARYING_CHARACTER_255;
+        else if(str === "NCHAR(55)") return Datatype.NCHAR_55;
+        else if(str === "NATIVE CHARACTER(70)") return Datatype.NATIVE_CHARACTER_70;
+        else if(str === "NVCHAR(100)") return Datatype.NVCHAR_100;
         else if(str === "TEXT") return Datatype.TEXT;
         else if(str === "CLOB") return Datatype.CLOB;
         // None
@@ -37,12 +37,59 @@ export class DatatypeHelper {
         else if(str === "FLOAT") return Datatype.FLOAT;
         // Numeric
         else if(str === "NUMERIC") return Datatype.NUMERIC;
-        else if(str === "DECIMAL_10_5") return Datatype.DECIMAL_10_5;
+        else if(str === "DECIMAL(10,5)") return Datatype.DECIMAL_10_5;
         else if(str === "BOOLEAN") return Datatype.BOOLEAN;
         else if(str === "DATE") return Datatype.DATE;
         else if(str === "DATETIME") return Datatype.DATETIME;
         // Error
         else throw new Error(`Unknown datatype passed: ${str}`);
+    }
+
+    /**
+     * Returns the string corresponding to a datatype value.
+     * @param datatype 
+     */
+    public static DatatypeToString(datatype: Datatype) {
+
+        // Integer
+        if(datatype === Datatype.INT) return "INT";
+        else if(datatype === Datatype.INTEGER) return "INTEGER";
+        else if(datatype === Datatype.TINYINT) return "TINYINT";
+        else if(datatype === Datatype.SMALLINT) return "SMALLINT";
+        else if(datatype === Datatype.MEDIUMINT) return "MEDIUMINT";
+        else if(datatype === Datatype.BIGINT) return "BIGINT";
+        else if(datatype === Datatype.INT2) return "INT2";
+        else if(datatype === Datatype.INT8) return "INT8";
+
+        // Text
+        else if(datatype === Datatype.CHARACTER_20) return "CHARACTER(20)";
+        else if(datatype === Datatype.VARCHAR_255) return "VARCHAR(255)";
+        else if(datatype === Datatype.VARYING_CHARACTER_255) return "VARYING CHARACTER(255)";
+        else if(datatype === Datatype.NCHAR_55) return "NCHAR(55)";
+        else if(datatype === Datatype.NATIVE_CHARACTER_70) return "NATIVE CHAR(70)";
+        else if(datatype === Datatype.NVCHAR_100) return "NVCHAR(100)";
+        else if(datatype === Datatype.TEXT) return "TEXT";
+        else if(datatype === Datatype.CLOB) return "CLOB";
+
+        // NONE
+        else if(datatype === Datatype.BLOB) return "BLOB";
+        else if(datatype === Datatype.UNSPECIFIED) return "";
+
+        // Real
+        else if(datatype === Datatype.REAL) return "REAL";
+        else if(datatype === Datatype.DOUBLE) return "DOUBLE";
+        else if(datatype === Datatype.DOUBLE_PRECISION) return "DOUBLE PRECISION";
+        else if(datatype === Datatype.FLOAT) return "FLOAT";
+
+        // Numeric
+        else if(datatype === Datatype.NUMERIC) return "NUMERIC";
+        else if(datatype === Datatype.DECIMAL_10_5) return "DECIMAL(10,5)";
+        else if(datatype === Datatype.BOOLEAN) return "BOOLEAN";
+        else if(datatype === Datatype.DATE) return "DATE";
+        else if(datatype === Datatype.DATETIME) return "DATETIME";
+
+        // Error
+        else throw new Error(`Unsupported datatype received "${datatype}"`);
     }
 
 }
